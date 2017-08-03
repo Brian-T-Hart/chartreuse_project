2e8535ee7e31e5420311adf2feca8597d3d11cf1
prathibha <prathibha.panga@gmail.com>
2017-08-02 20:27:03 -0700

    updated files added

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+function dayAndNight(){
+
+	var current = new Date();
+	var day_night = current.getHours();
+
+		if (day_night < 12){
+			//Day
+			
+			document.body.style.backgroundImage = "url('assets/images/sunrise.jpg')";
+		}
+		else{
+			//Night
+			
+				document.body.style.backgroundImage = "url('assets/images/night.jpg')";
+		}
+}
+dayAndNight();
